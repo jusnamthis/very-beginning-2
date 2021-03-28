e8c5eee854ae21cbc95e381a44b3cf304961336d
Vyach <KartashovVD@yandex.ru>
2021-03-28 19:04:23 +0300

    loaded all tasks

--- task4.py
+++ task4.py
@@ -1,37 +1,11 @@
-your_dict = {}
+# 4. Представлен список чисел. Необходимо вывести те его элементы, значения которых больше предыдущего, например:
+#
+# src = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+# result = [12, 44, 4, 10, 78, 123]
+#
+# Подсказка: использовать возможности python, изученные на уроке.
 
 
-def create_list(key_word, names):
-    _names_list = []
-    for name in names:
-        if name[0] == key_word[0]:
-            _names_list.append(name)
-    return _names_list
-
-
-def thesaurus(*args):
-    result = {}
-    for name in args:
-        result.setdefault(name[0], create_list(name, args))
-    return result
-
-
-print(thesaurus('Иван', 'Илья', 'Мария', 'Пётр'))
-
-
-def thesaurus_adv(*args):
-    global your_dict
-    for bio in args:
-        name, surname = bio.split()
-        if your_dict.get(surname[0]) == None:
-            your_dict.setdefault(surname[0], thesaurus(bio))
-        else:
-            _ = your_dict.get(surname[0]) # этой строчкой получаю доступ к словарю внутреннему (по именам)
-            bykeylist = _.setdefault(name[0], []) # получаю значение по ключу в этом словаре для того чтобы далее записывать туда новые значения
-            bykeylist.append(bio) # запись значения
-    return your_dict
-
-
-print(thesaurus_adv("Иван Сергеев", "Инна Серова", "Петр Алексеев", "Илья Иванов", "Анна Савельева", "Екатерина Сапирова"))
-
-
+src = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+bigger_than_prev = [src[i] for i in range(1, len(src)) if src[i] > src[i-1]]
+print(bigger_than_prev)
