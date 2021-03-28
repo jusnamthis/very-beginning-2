ff010be671a497d6509f74fd0bd0f2a2b761685a
Vyach <KartashovVD@yandex.ru>
2021-03-28 19:17:53 +0300

    loaded all files and tasks

---
+++ task1.py
@@ -0,0 +1,27 @@
+# 1. Не используя библиотеки для парсинга, распарсить (получить определённые данные) файл логов web-сервера
+# nginx_logs.txt
+#
+# (https://github.com/elastic/examples/raw/master/Common%20Data%20Formats/nginx_logs/nginx_logs) — получить список
+# кортежей вида: (<remote_addr>, <request_type>, <requested_resource>). Например:
+#
+# [
+# ...
+# ('141.138.90.60', 'GET', '/downloads/product_2'),
+# ('141.138.90.60', 'GET', '/downloads/product_2'),
+# ('173.255.199.22', 'GET', '/downloads/product_2'),
+# ...
+# ]
+#
+# Примечание: код должен работать даже с файлами, размер которых превышает объем ОЗУ компьютера.
+
+
+def create_tuple(log_list):
+    for i in range(0, len(log_list)):
+        return log_list[0], log_list[5], log_list[6]
+
+
+result = []
+with open('nginx_logs.txt', 'r', encoding='utf-8') as f:
+    i = 0
+    for line in f:
+        result.append(create_tuple(line.split()))
